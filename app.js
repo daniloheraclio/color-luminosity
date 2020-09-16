09076176b0bcf2cf37a53346feef7b522725572e
Danilo Heraclio <emaildedanilo@gmail.com>
2020-09-16 15:56:34 -0400

    create logic

---
+++ app.js
@@ -0,0 +1,62 @@
+const { body } = document;
+
+try {
+   body.style.backgroundColor = luminace('#63c', -0.6);
+} catch (error) {
+   console.log(error.message);
+}
+
+
+function luminace(hex, luminosity = 0) {
+
+   // The hexadecial range is 0 to f
+   // It has 16 digits
+   // 0 = black
+   // f = white
+
+   // format hex to keep only numbers and letters from a to f
+   hex = hex.replace(/[^0-9a-f]/gi, '');
+
+   // accept only 3 or 6 digits
+   const isValidHex = hex.length === 6 || hex.length === 3;
+   if(!isValidHex) throw new Error('Invalid HEX');
+
+
+   // if 3 digits transform to 6
+   if (hex.length === 3) {
+      hex = hex[0] + hex[0] + hex[1] + hex[1] + hex[2] + hex[2];
+   }
+
+
+   // transform hex in rgb to apply the lumiance
+   // r (red) / g (green) / b (blue)
+   // 0 = black
+   // 255 = white
+
+   const twoDigitGroup = hex.match(/([0-9a-f]){2}/gi)
+
+   let finalHex = '#';
+   twoDigitGroup.forEach(twoDigit => {
+      const numberFromHex = parseInt(twoDigit, 16);
+      const calculateLuminosity = numberFromHex + (luminosity * 255);
+
+      // Limit number to be between 0 and 255
+      const blackOrLuminosity = Math.max(0, calculateLuminosity);
+      const partialColor = Math.min(255, blackOrLuminosity);
+
+
+      // Get rounded number
+      const newColor = Math.round(partialColor);
+
+      // transform to string base16
+      const numberToHex = newColor.toString(16);
+
+      // insert 0 before string
+      const partialHex = numberToHex.padStart(2, '0');
+
+      finalHex = finalHex + partialHex;
+      console.log(finalHex);
+
+   });
+   return finalHex
+}
